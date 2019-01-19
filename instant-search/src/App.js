dc0ce490236b7ee3584e94abcc11d60d3c8ae94a
boo <ya.ternovskiy@gmail.com>
2019-01-19 21:13:04 +0300

    Search results

--- instant-search/src/App.js
+++ instant-search/src/App.js
@@ -2,22 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { InstantSearch, SearchBox } from 'react-instantsearch-dom';
+import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch-dom';
+
+const Hit = ({hit}) =>
+  <div className="hit">
+    <div className="hitImage">
+      <img src={hit.photo_urls[0]}/>
+    </div>
+      <div className = "hitName">
+        <Highlight attribute = "name" hit = {hit}/>
+      </div>
+  </div>
+
+const Content = () =>
+  <div className = "content">
+    <Hits hitComponent = {Hit}/>
+</div>
 
 class App extends Component {
   render() {
     return (
       <InstantSearch
-        appId="instant-search"
+        appId="6418JXEJAP"
         apiKey="cee5283ec0521081916c75ad09156f13"
-        indexName="bestbuy"
+        indexName="movies"
       >
           <header>
             Instant Search
+            <SearchBox translations = {{placeholder:'Search Box'}}/>
           </header>
-
-          <SearchBox translations = {{placeholder:'Search Box'}}/>
-
+         
+          <main>
+            <Content/>
+          </main>
       </InstantSearch>
     );
   }
